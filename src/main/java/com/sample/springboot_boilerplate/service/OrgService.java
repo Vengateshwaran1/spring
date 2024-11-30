18c2946406db917fd4708aeb4997709b626c87af
Vengateshwaran1 <kvengateshwaran1@gmail.com>
2024-11-30 11:59:30 +0530

    commit

--- src/main/java/com/sample/springboot_boilerplate/service/OrgService.java
+++
@@ -1,8 +0,0 @@
-package com.sample.springboot_boilerplate.service;
-
-import com.sample.springboot_boilerplate.dto.OrgDto;
-import java.util.List;
-
-public interface OrgService {
-    List<OrgDto> listOrgs();
-}
